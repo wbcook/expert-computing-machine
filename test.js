84124b1320024bd451632b912d1af1c9fc87fe11
William Cook <wbcook007@gmail.com>
2016-07-20 15:17:54 -0400

    tests

--- test.js
+++ test.js
@@ -54,20 +54,20 @@ describe('Chat', function() {
     chat.should.be.ok;
   });
 
-  describe('messages()', function() {
+  describe('message()', function() {
 
     it('should get messages from the chat history', function() {
       var chat = new Chat();
       var message = 'old message';
-      chat.set(message);
-      chat.get().should.equal(message);
+      chat.message(message);
+      chat.message().should.equal(message);
     });
 
-    it('should set messages to the chat history', function() {
+    it('should set messages from the chat history', function() {
       var chat = new Chat();
-      var message = 'new message';
-      chat.set(message);
-      chat.get().should.equal(message);
+      var message = 'old message';
+      chat.message(message);
+      chat.message().should.equal(message);
     });
 
   });
@@ -105,15 +105,15 @@ describe('Lobby', function() {
     it('should get the players in the lobby', function() {
       var lobby = new Lobby();
       var player = 'player1';
-      lobby.set(player);
-      lobby.get().should.equal(player);
+      lobby.players(player);
+      lobby.players().should.equal(player);
     });
 
     it('should set the players in the lobby', function() {
       var lobby = new Lobby();
-      var player = 'player2';
-      lobby.set(player);
-      lobby.get().should.equal(player);
+      var player = 'player1';
+      lobby.players(player);
+      lobby.players().should.equal(player);
     });
 
   });
